24e49c0acea4f69ca98ef7f020a8d5421d8ebec4
MAASIFAR <maasifar@gmail.com>
2024-08-18 14:15:49 +0530

    some

--- File_Sorter/main.py
+++ File_Sorter/main.py
@@ -3,26 +3,35 @@ import shutil
 
 def folder_path(path):
     log=[]
+    subfolder_checker=[]
     for folder,subfolder,files in os.walk(path):
-         for sub in subfolder:
-            print(sub)
-            if files and sub not in log:
-                for file_name in files:
-                    spliter=file_name.split('.')
-                    print(log)
-                    if spliter[1] not in log:
-                        os.mkdir(path+"\\"+spliter[1])
-                        shutil.move(folder+"\\"+files[0],path+"\\"+spliter[1])
-                        log.append(spliter[1].upper)
-                    else:
-                        shutil.move(folder+"\\"+files[0],path+"\\"+spliter[1])
-    # for folder,subfolder,files in os.walk(path):
-    #     if subfolder[0] not in log:
-    #         os.rmdir(path+"\\"+subfolder[0])
+      main_Path=path
+      if subfolder:
+        for i in subfolder:
+            subfolder_checker.append(i.lower())
+      if files:
+          for i in files:
+              if i.split('.')[1].lower() in subfolder_checker:
+                  shutil.move(folder+"\\"+i,folder+"\\"+i.split('.')[1].lower()) 
+                  log.append(i.split('.')[1].lower())
+              else:
+                  os.mkdir(main_Path+"\\"+i.split('.')[1].lower())
+                  shutil.move(folder+"\\"+i,main_Path+"\\"+i.split('.')[1].lower()+i)
+                  log.append(i.split('.')[1].lower())
+                  subfolder_checker.append(i.split('.')[1].lower())
+                  
+            
+           
+                
+            
+               
     
             
                 
                 
-folder_path("C:\\Users\\JOVE\\OneDrive\\Documents\\WORKSPACE\\python\\PROJECTS\\File_Sorter\\test")
+folder_path("C:\\Users\\JOVE\\Music\\test")
+
+
+
 
        
