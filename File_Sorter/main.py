ae5824760203a747e6d1c7a9fba4a83af44a39e5
MAASIFAR <maasifar@gmail.com>
2024-08-20 14:07:38 +0530

    almost

--- File_Sorter/main.py
+++ File_Sorter/main.py
@@ -1,24 +1,63 @@
 import os
 import shutil
 
+
+LOG=[]
 def folder_path(path):
-    log=[]
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"The folder {path} does not exist.")
+    
     subfolder_checker=[]
+    
+    for folder,subfolder,files in os.walk(path):
+           for sub in subfolder:
+               subfolder_checker.append(sub.lower())
+           break
+    LOG=subfolder_checker.copy()
+    print(LOG)
+       
+   
+    
+    
     for folder,subfolder,files in os.walk(path):
       main_Path=path
-      if subfolder:
-        for i in subfolder:
-            subfolder_checker.append(i.lower())
+      
+      
       if files:
           for i in files:
-              if i.split('.')[1].lower() in subfolder_checker:
-                  shutil.move(folder+"\\"+i,folder+"\\"+i.split('.')[1].lower()) 
-                  log.append(i.split('.')[1].lower())
-              else:
+               count=0
+               if i.split('.')[1].lower() in subfolder_checker:
+                  temp=i.split('.')[1].lower()
+                  
+                  
+                  if folder+"\\" !=main_Path+"\\"+i.split('.')[1].lower()+"\\":
+                     shutil.move(folder+"\\"+i,main_Path+"\\"+i.split('.')[1].lower()+"\\") 
+                     
+                    
+                    
+               else:
                   os.mkdir(main_Path+"\\"+i.split('.')[1].lower())
-                  shutil.move(folder+"\\"+i,main_Path+"\\"+i.split('.')[1].lower()+i)
-                  log.append(i.split('.')[1].lower())
-                  subfolder_checker.append(i.split('.')[1].lower())
+                  shutil.move(folder+"\\"+i,main_Path+"\\"+i.split('.')[1].lower()+"\\")
+                  temp=i.split('.')[1].lower()
+                  subfolder_checker.append(temp)
+                  
+                  LOG.append(temp)
+    print(LOG,subfolder_checker)
+
+# def delete_folder(path):
+#     if not os.path.exists(path):
+#         raise FileNotFoundError(f"The folder {path} does not exist.")
+#     log=[]
+#     for files,sub_folder,folder in  os.walk(path):
+                
+#                     for i in sub_folder:
+                         
+            
+        
+    
+    
+   
+    
                   
             
            
@@ -30,6 +69,7 @@ def folder_path(path):
                 
                 
 folder_path("C:\\Users\\JOVE\\Music\\test")
+# delete_empty("C:\\Users\\JOVE\\Music\\test")
 
 
 
---
+++ File_Sorter/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+ #   else:
+            #       os.mkdir(main_Path+"\\"+i.split('.')[1].lower())
+            #       shutil.move(folder+"\\"+i,main_Path+"\\"+i.split('.')[1].lower()+i)
+            #       log.append(i.split('.')[1].lower())
+            #       subfolder_checker.append(i.split('.')[1].lower())
