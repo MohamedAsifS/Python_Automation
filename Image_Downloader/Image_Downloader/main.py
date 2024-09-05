e7c5af9abb547c132b827e6a0b1f85264191ac51
MAASIFAR <maasifar@gmail.com>
2024-09-05 13:46:48 +0530

    New

--- Image_Downloader/Image_Downloader/main.py
+++ Image_Downloader/Image_Downloader/main.py
@@ -1,33 +1,38 @@
-import requests,bs4
+import requests,bs4,os
 
 
 # get_the_link=input("Enter The Word : ")
 
-def get_the_page(get_the_link):
-    actual_link=f"https://www.google.com//images?q={get_the_link}"
+def get_the_Search():
+    cse_id="605b2f7696f594e3b"
     
-    get_respose=requests.get(actual_link)
+def get_path_save_file(path,file_name):
+    os.mkdir(path+"\\"+file_name)
+
+
+
+# beauty=bs4.BeautifulSoup(get_the_page("virat kholi"),'lxml')
+
+# img=beauty.find_all('img')
+
+# for i in img:
+#     if i['alt']=='Google':
+#         pass
+#     else:
+#       img_link=requests.get(i['src'])
+#       if img_link.status_code==200:
+#         f=open("dhoni1.jpg",'wb')
+#         f.write(img_link.content)
+#         f.close()
+
+
     
-    return get_respose.content
-
-beauty=bs4.BeautifulSoup(get_the_page("virat kholi"),'lxml')
-
-img=beauty.find_all('img')
-
-for i in img:
-    if i['alt']=='Google':
-        pass
-    else:
-      img_link=requests.get(i['src'])
-      if img_link.status_code==200:
-        f=open("dhoni1.jpg",'wb')
-        f.write(img_link.content)
-        f.close()
-       
+    
+    
     
 
 # print(get_the_page("cat"))
-
+# get_path_save_file("C:\\Users\\JOVE\\OneDrive\\Documents\\WORKSPACE\\python\\PROJECTS\\Image_Downloader\\Image_Downloader","cat")
 # to download image
 
 # print(img_link.content)->in image.content  method has bianary of an contents
