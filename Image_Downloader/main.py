67e4f1fb272bbd9ffcabf51f83805d5d805c94a5
MAASIFAR <maasifar@gmail.com>
2024-09-05 13:43:04 +0530

    new one

---
+++ Image_Downloader/main.py
@@ -0,0 +1,53 @@
+import requests,bs4,os
+
+
+# get_the_link=input("Enter The Word : ")
+
+def get_the_Search():
+    cse_id="605b2f7696f594e3b"
+    
+def get_path_save_file(path,file_name):
+    os.mkdir(path+"\\"+file_name)
+
+
+
+# beauty=bs4.BeautifulSoup(get_the_page("virat kholi"),'lxml')
+
+# img=beauty.find_all('img')
+
+# for i in img:
+#     if i['alt']=='Google':
+#         pass
+#     else:
+#       img_link=requests.get(i['src'])
+#       if img_link.status_code==200:
+#         f=open("dhoni1.jpg",'wb')
+#         f.write(img_link.content)
+#         f.close()
+
+
+    
+    
+    
+    
+
+# print(get_the_page("cat"))
+# get_path_save_file("C:\\Users\\JOVE\\OneDrive\\Documents\\WORKSPACE\\python\\PROJECTS\\Image_Downloader\\Image_Downloader","cat")
+# to download image
+
+# print(img_link.content)->in image.content  method has bianary of an contents
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
---
+++ Image_Downloader/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+for i in img:
+#     if i['alt']=='Google':
+#         pass
+#     else:
+#       img_link=requests.get(i['src'])
+#       if img_link.status_code==200:
+#         f=open("dhoni1.jpg",'wb')
+#         f.write(img_link.content)
+#         f.close()
